d6fdea9db09d8781bfb2dc358b7dc2c40f5c3aa1
David Schöne <david.blaa@googlemail.com>
2021-12-20 14:53:49 +0100

    BEXIS2/Core#818 Add FileIcon component to show icons for a file based on type

---
+++ src/components/File/FileIcon.svelte
@@ -0,0 +1,53 @@
+<script>
+import Fa from 'svelte-fa/src/fa.svelte'
+import { faFileAudio } from '@fortawesome/free-solid-svg-icons'
+import { faFileVideo } from '@fortawesome/free-solid-svg-icons'
+import { faFileWord } from '@fortawesome/free-solid-svg-icons'
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
+import { faFileExcel } from '@fortawesome/free-solid-svg-icons'
+import { faFileCsv } from '@fortawesome/free-solid-svg-icons'
+import { faFileImage } from '@fortawesome/free-solid-svg-icons'
+import { faFileArchive } from '@fortawesome/free-solid-svg-icons'
+
+ export let type = "";
+ 
+</script>
+
+{#if type.includes("excel")}
+  <Fa icon={faFileExcel} />
+{/if}
+
+{#if type.includes("text")}
+  <Fa icon={faFileCsv} />
+{/if}
+
+{#if type.includes("image")}
+  <Fa icon={faFileImage} />
+{/if}
+
+{#if type.includes("audio")}
+  <Fa icon={faFileAudio} />
+{/if}
+
+{#if type.includes("video")}
+  <Fa icon={faFileVideo} />
+{/if}
+
+{#if type.includes("word")}
+  <Fa icon={faFileWord} />
+{/if}
+
+{#if type.includes("pdf")}
+  <Fa icon={faFilePdf} />
+{/if}
+
+{#if type.includes("zip")}
+  <Fa icon={faFileArchive} />
+{/if}
+<style>
+  
+div{
+  font-size: large;
+}
+</style>
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from '../src/components/Button/Button.svelte';
 export { default as Treeview } from '../src/components/Treeview/Treeview.svelte';
+export { default as FileIcon } from '../src/components/File/FileIcon.svelte';
